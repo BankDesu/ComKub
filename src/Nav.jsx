ce778f12cebc19a0aef08c73fcfae9845e050a5f
Guy-24 <Bombboom24092546@gmail.com>
2024-05-11 05:24:44 +0700

    add account-btn

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -5,6 +5,9 @@ import paper2 from "./assets/paper2.png";
 import review from "./assets/review.png";
 import category from "./assets/category.png";
 import search from "./assets/search.png";
+import account from "./assets/account.png";
+import light from "./assets/light.png";
+import dark from "./assets/dark.png";
 
 function Nav() {
   return (
@@ -28,6 +31,16 @@ function Nav() {
           </button>
           <img className="search-icon" src={search} />
           <input className="search-bar" type="search" />
+        </div>
+        <div className="darkmode-section">
+          <button className="darkmode-btn">
+            <img className="darkmode-icon" src={dark} />
+          </button>
+        </div>
+        <div className="account-section">
+          <button className="account-btn">
+            <img className="account-icon" src={account} />
+          </button>
         </div>
       </nav>
     </>
