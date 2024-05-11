b9d56d962ab31ed1aad24d61fca9b07095cf4538
Guy-24 <Bombboom24092546@gmail.com>
2024-05-11 19:44:52 +0700

    .

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -6,35 +6,36 @@ import review from "./assets/review.png";
 import category from "./assets/category.png";
 import search from "./assets/search.png";
 import account from "./assets/account.png";
-import light from "./assets/light.png";
-import dark from "./assets/dark.png";
+import moon from "./assets/moon.png";
 
 function Nav() {
   return (
     <>
       <nav>
         <a href="#">
-          <img src={comkub} height="150" />
+          <img className="comkub" src={comkub} height="150" />
         </a>
         {/*Home */}
         <a href="#">
-          <img src={paper2} height="150" />
+          <img className="article" src={paper2} height="150" />
         </a>{" "}
         {/* Article */}
         <a href="#">
-          <img src={review} height="150" />
+          <img className="review" src={review} height="150" />
         </a>
         {/* Review */}
         <div className="search-section">
           <button className="category-btn">
-            <img className="category" src={category}></img>
+            <img className="category-icon" src={category}></img>
           </button>
-          <img className="search-icon" src={search} />
-          <input className="search-bar" type="search" />
+          <div className="searchbar-section">
+            <img className="search-icon" src={search} />
+            <input className="search-bar" type="search" />
+          </div>
         </div>
         <div className="darkmode-section">
           <button className="darkmode-btn">
-            <img className="darkmode-icon" src={dark} />
+            <img className="darkmode-icon" src={moon} />
           </button>
         </div>
         <div className="account-section">
