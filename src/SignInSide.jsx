4e99296ca175cd0324b271b5fe68ab096fd95e59
Guy-24 <Bombboom24092546@gmail.com>
2024-05-26 22:52:30 +0700

    sign in - sign up

--- src/SignInSide.jsx
+++ src/SignInSide.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import "./SignInSide.css";
 import * as React from "react";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
@@ -16,18 +17,32 @@ import { createTheme, ThemeProvider } from "@mui/material/styles";
 const defaultTheme = createTheme();
 
 export default function SignInSide() {
+  const [checked, setChecked] = React.useState(true);
+
+  const handleChange = () => {
+    setChecked((prev) => !prev);
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     console.log({
       email: data.get("email"),
       password: data.get("password"),
+      name: data.get("name"),
+      email2: data.get("email2"),
+      password2: data.get("password2"),
+
     });
   };
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Grid container component="main" sx={{ height: "100vh" }}>
+      <Grid className="SideSignIn"
+        container
+        component="main"
+        sx={{ height: "100vh", position: "relative" }}
+      >
         <CssBaseline />
         <Grid
           item
@@ -35,8 +50,6 @@ export default function SignInSide() {
           sm={4}
           md={7}
           sx={{
-            backgroundImage:
-              "url(https://wallpapers.com/images/featured/laptop-murjp1nk4lp1idlt.jpg)",
             backgroundRepeat: "no-repeat",
             backgroundColor: (t) =>
               t.palette.mode === "light"
@@ -44,6 +57,15 @@ export default function SignInSide() {
                 : t.palette.grey[900],
             backgroundSize: "cover",
             backgroundPosition: "center",
+            position: "absolute",
+            backgroundImage: checked
+              ? "url(https://wallpapers.com/images/featured/laptop-murjp1nk4lp1idlt.jpg)"
+              : "url(https://images6.alphacoders.com/133/1338694.png)",
+            top: "0%",
+            left: checked ? "0%" : "41.7%",
+            zIndex: checked ? "2" : "2",
+            width: "100%",
+            height: "100%",
           }}
         />
         <Grid
@@ -56,10 +78,14 @@ export default function SignInSide() {
           square
           sx={{
             height: "100vh",
-            background: "linear-gradient(to right, #D9E7FE, #F7FAFE)",
+            background: "linear-gradient(to right, #ffd8b5, #daf0f7)",
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
+            position: "absolute",
+            top: 0,
+            right: 0,
+            zIndex: checked ? "3" : "1",
           }}
         >
           <Box
@@ -71,20 +97,21 @@ export default function SignInSide() {
               alignItems: "center",
             }}
           >
-            <Link to="/">
+            <Link to="/">  
               <Box
                 sx={{
                   position: "absolute",
                   top: 10,
-                  left: 10,
+                  left: -950,
                   m: 2,
+                  zIndex: "10",
                 }}
               >
                 <HomeIcon sx={{ color: "white", fontSize: 50 }} />
               </Box>
             </Link>
             <Avatar
-              sx={{ m: 1, bgcolor: "rgb(63, 175, 209)", width: 55, height: 55 }}
+              sx={{ m: 1, bgcolor: "#ff6835", width: 55, height: 55 }}
             ></Avatar>
             <Typography
               component="h1"
@@ -133,14 +160,129 @@ export default function SignInSide() {
               </Button>
               <Grid container sx={{ marginTop: 4 }}>
                 <Grid item xs>
-                  <Link href="#" variant="body2">
-                    Forgot password?
-                  </Link>
+                  <Button disabled>
+                    <Typography variant="body2">
+                      {"Forgot password?"}
+                    </Typography>
+                  </Button>
                 </Grid>
                 <Grid item>
-                  <Link href="#" variant="body2">
-                    {"Don't have an account? Sign Up"}
-                  </Link>
+                  <Button variant="text" onClick={handleChange}>
+                    <Typography variant="body2">
+                      {"Don't have an account? Sign Up"}
+                    </Typography>
+                  </Button>
+                </Grid>
+              </Grid>
+            </Box>
+          </Box>
+        </Grid>
+
+
+
+        <Grid
+          item
+          xs={12}
+          sm={8}
+          md={5}
+          component={Paper}
+          elevation={6}
+          square
+          sx={{
+            height: "100vh",
+            background: "linear-gradient(to right,#f2c4e6,#c3f3f7)",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            position: "absolute",
+            top: 0,
+            left: 0,
+            zIndex: checked ? "1" : "3",
+          }}
+        >
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Link to="/">
+              <Box
+                sx={{
+                  position: "absolute",
+                  top: 10,
+                  left: 45,
+                  m: 2,
+                  zIndex: "5",
+                }}
+              >
+                <HomeIcon sx={{ color: "black", fontSize: 50 }} />
+              </Box>
+            </Link>
+            <Avatar
+              sx={{ m: 1, bgcolor: "#fd43c8", width: 55, height: 55 }}
+            ></Avatar>
+            <Typography
+              component="h1"
+              variant="h5"
+              sx={{ fontSize: 35, marginTop: 2 }}
+            >
+              Sign up
+            </Typography>
+            <Box
+              component="form"
+              noValidate
+              onSubmit={handleSubmit}
+              sx={{ mt: 1 }}
+            >
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="name"
+                label="Name"
+                name="name"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email2"
+                label="Email Address"
+                name="email2"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password2"
+                label="Password"
+                type="password"
+                id="password2"
+                autoComplete="current-password"
+              />
+
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign Up
+              </Button>
+              <Grid container sx={{ marginTop: 4 }}>
+                <Grid item>
+                  <Button variant="text" onClick={handleChange}>
+                    <Typography variant="body2">
+                      {"Already have an account? Sign In"}
+                    </Typography>
+                  </Button>
                 </Grid>
               </Grid>
             </Box>
