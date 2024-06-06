09519c88f3c31fa0827218defe218a509fec86bb
BankDesu <149315970+BankDesu@users.noreply.github.com>
2024-06-07 02:46:07 +0700

    .env

--- Backend/config/dbConnection.js
+++ Backend/config/dbConnection.js
@@ -1,11 +1,14 @@
+import dotenv from 'dotenv';
 import mysql from 'mysql2';
 
+dotenv.config();
+
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'comkub',
-    port: 3306
+    host: process.env.DB_HOST ,
+    user: process.env.DB_USER ,
+    password: process.env.DB_PASS ,
+    database: process.env.DB_NAME ,
+    port: process.env.DB_PORT
 });
 
 db.connect(err => {
