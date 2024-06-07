ebbed8ad4f59c9e3e35f570096be2cf92fc62896
BankDesu <149315970+BankDesu@users.noreply.github.com>
2024-06-07 19:48:39 +0700

    import cors

--- Backend/index.js
+++ Backend/index.js
@@ -1,12 +1,30 @@
+import cors from 'cors';
 import express from 'express';
 import notebookRoutes from './routes/notebookRoutes.js';
 import userReviewRoutes from './routes/userReviewRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 
+
 const app = express();
 const port = 4000;
 
+const corsOptions = {
+    origin: [
+        'http://localhost',
+        'http://localhost:4000',
+        'http://localhost:5173',
+
+    ],
+    credentials: true,
+    optionSuccessStatus: 200
+};
+app.use(cors(corsOptions));
 app.use(express.json());
+
+app.all("/", (req, res) => {
+    res.sendStatus(200)
+});
+
 app.use('/user', userRoutes);
 app.use('/review', userReviewRoutes);
 app.use('/notebook', notebookRoutes);
