a4996e206ef63f40865eaee1d3425ebbbf040059
BankDesu <149315970+BankDesu@users.noreply.github.com>
2024-06-10 03:11:14 +0700

    Update comkubdatabase.sql

--- comkubdatabase.sql
+++ comkubdatabase.sql
@@ -34,15 +34,23 @@ create table review (
 	reviewid int auto_increment not null primary key,
 	userid int not null ,
     notebook_id int not null,
-    review_title varchar(255),
-    perfromance_score FLOAT,
+    performance_score FLOAT,
     service_score FLOAT,
 	FOREIGN KEY (userid) REFERENCES user(userid),
 	FOREIGN KEY (notebook_id) REFERENCES notebook(notebook_id)
 );
 -- drop table review;
--- drop table review;
  
- -- INSERT INTO user (username,password,birthday,gender,email) VALUES ('BankDesu','bank2739','2003-07-01','Male',''); 
-
+ -- INSERT INTO user (username,password,email) VALUES ('BankDesu','bank2739','psuwimonopas@gmail.com'); 
+ 
  select * from user ;
+ 
+ insert into notebook (brand,model,model_year,notebook_name,category,price,cpu,gpu,ram,display,storage,os,pic_path) values
+(
+	'MSI','Prestige 16 AI Studio B1VFG-028TH','Jan 2024','MSI Prestige 16 AI Studio B1VFG-028TH','gaming',
+	72990,'Intel Core Ultra 7 155H','NVIDIA GeForce RTX 4060','32 GB LPDDR5 On Board6400 MHz',
+	'16.0 inch, QHD (2560 x 1600)','1TB SSD PCIe M.2 Gen 4','Windows 11 Home','https://notebookspec.com/resize/648x486//storage/notebook/13239-1.png'
+);
+select * from notebook;
+
+select * from review;
